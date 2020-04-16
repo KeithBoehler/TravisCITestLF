6ef3730b49edbebde2b07cd171b0228173617d77
Keith Boehler <germanbohler@gmail.com>
2020-04-15 21:21:43 -0500

    Some tests

---
+++ travisCI.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 15 20:56:04 2020
+
+@author: keithnator3000
+
+Some code to test an implementation for Travis CI.
+
+Converting LoFASM Py2 code to Py3
+"""
+
+def simpleAdd(num):
+    return num + 5
+
+
+print("Hello world!")
+
+x = simpleAdd(4)
+
+print("Simple add has returned " + str(x))
